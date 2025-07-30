142f467ce766e8f43847e74e3321f7aa8d687a2a
saeedvaziry <61919774+saeedvaziry@users.noreply.github.com>
2025-07-30 14:21:08 +0000

    Update version to 3.0.0-beta-5

--- config/app.php
+++ config/app.php
@@ -218,7 +218,7 @@ return [
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
-    'version' => '3.0.0-beta-4',
+    'version' => '3.0.0-beta-5',
 
     'demo' => env('APP_DEMO', false),
 
