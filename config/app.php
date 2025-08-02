5cb39ea39cf9e42dff0d60f7ae721776d64c2c4a
Saeed Vaziry <61919774+saeedvaziry@users.noreply.github.com>
2025-08-02 12:25:00 +0200

    Update app.php

--- config/app.php
+++ config/app.php
@@ -218,7 +218,7 @@ return [
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
-    'version' => '3.0.0-beta-7',
+    'version' => '3.0.0',
 
     'demo' => env('APP_DEMO', false),
 
