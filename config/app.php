3b39dd4ffc2fd62c7984f1f88785fb5b03e5b0de
saeedvaziry <61919774+saeedvaziry@users.noreply.github.com>
2025-08-09 20:36:44 +0000

    Update version to 3.1.1

--- config/app.php
+++ config/app.php
@@ -218,7 +218,7 @@ return [
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
-    'version' => '3.1.0',
+    'version' => '3.1.1',
 
     'demo' => env('APP_DEMO', false),
 
