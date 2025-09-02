77a69d6f2fc2e2c3e7cb8176f0cb68e8cc529a21
saeedvaziry <61919774+saeedvaziry@users.noreply.github.com>
2025-09-02 17:56:48 +0000

    Build version 3.6.0

---
+++ public/build/assets/skeleton-BTe1Gty3.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-CQ9a6rAn.js";import{c as s}from"./utils-CJcywj8I.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
