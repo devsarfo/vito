976c68ac073dcbf0fb8762fa3900bc12e25aded8
saeedvaziry <61919774+saeedvaziry@users.noreply.github.com>
2025-09-29 09:03:30 +0000

    Build version 3.9.0-beta-1

--- config/app.php
+++ config/app.php
@@ -218,7 +218,7 @@ return [
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
-    'version' => '3.8.0',
+    'version' => '3.9.0-beta-1',
 
     'demo' => env('APP_DEMO', false),
 
---
+++ public/build/assets/useQuery-B3X52EZo.js
@@ -0,0 +1,1 @@
+import{u as r,l as u}from"./useBaseQuery-B_XZKTIU.js";function a(e,s){return r(e,u)}export{a as u};
