c175770eac94087e726ebcb62ee6b5dc6e4677cf
saeedvaziry <61919774+saeedvaziry@users.noreply.github.com>
2025-10-18 20:31:45 +0000

    Build version 3.13.0

--- config/app.php
+++ config/app.php
@@ -220,7 +220,7 @@ return [
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
-    'version' => '3.12.0',
+    'version' => '3.13.0',
 
     'demo' => env('APP_DEMO', false),
 
