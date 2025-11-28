e34d4325fade0daa4231e6e9ffca5ad5e4866a8f
saeedvaziry <61919774+saeedvaziry@users.noreply.github.com>
2025-11-28 17:58:49 +0000

    Build version 3.16.0

---
+++ public/build/assets/skeleton-C0ChX6FI.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-DH7zW309.js";import{c as s}from"./utils-0lA1DGtL.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
--- public/build/assets/skeleton-as6i2qa9.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-DfchsBom.js";import{c as s}from"./utils-DvDcXRbs.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
