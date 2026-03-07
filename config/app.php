ea15bccc65ae0bae86268156c9b1b6b76403c7a9
saeedvaziry <61919774+saeedvaziry@users.noreply.github.com>
2026-03-07 12:10:27 +0000

    Build version 4.0.0-beta-2

--- config/app.php
+++ config/app.php
@@ -220,7 +220,7 @@ return [
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
-    'version' => '4.0.0-beta-1',
+    'version' => '4.0.0-beta-2',
 
     'demo' => env('APP_DEMO', false),
 
---
+++ public/build/assets/useQuery-Bs_r-f2x.js
@@ -0,0 +1,1 @@
+import{u as r,l as u}from"./useBaseQuery-BByEse1r.js";function a(e,s){return r(e,u)}export{a as u};
