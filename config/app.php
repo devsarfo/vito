2d0abd0b49222bc01211732be5489c32363e1cc5
saeedvaziry <61919774+saeedvaziry@users.noreply.github.com>
2026-06-30 22:01:06 +0000

    Build version 4.0.0

--- config/app.php
+++ config/app.php
@@ -258,7 +258,7 @@ return [
         // 'ExampleClass' => App\Example\ExampleClass::class,
     ])->toArray(),
 
-    'version' => '4.0.0-beta-12',
+    'version' => '4.0.0',
 
     'demo' => env('APP_DEMO', false),
 
